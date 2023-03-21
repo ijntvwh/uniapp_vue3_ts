57afb1fc092c2fccafa41d3757dd0bb5253b7e64
wh <15996589@qq.com>
2023-03-21 14:24:19 +0800

    build: 插件更新

--- build/appid.ts
+++ build/appid.ts
@@ -6,13 +6,14 @@ const JSON_JS = JSON_JS_MAP['manifest.json']
 export const writeAppid: Plugin = {
   name: 'write-appid',
   configResolved(config) {
-    if (config.define['process.env.UNI_PLATFORM'] !== 'mp-weixin') return
+    if (config.define && config.define['process.env.UNI_PLATFORM'] !== '"mp-weixin"') return
     const _appid = config.env.VITE_MP_APPID
     if (!_appid) {
       console.log('环境变量 VITE_MP_APPID 未配置')
       process.exit(1)
     }
     const p = config.plugins.find(p => p.name === 'uni:mp-manifest-json')
+    if (!p) return
     const transform1 = p.transform as any
     p.transform = function (...args) {
       const id = args[1]
