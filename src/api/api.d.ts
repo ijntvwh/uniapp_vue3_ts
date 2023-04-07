156a7edbd11a7e6b87ccb2ccc98876421d588db0
wh <15996589@qq.com>
2023-04-07 17:08:19 +0800

    feat: 添加lock文件

--- src/api/api.d.ts
+++ src/api/api.d.ts
@@ -6,8 +6,7 @@ declare module 'uni-ajax' {
     custom?: ReqCustomKey[]
   }
   interface AjaxInvoke {
-    <T = any>(config?: AjaxRequestConfig): Request<T>
-    <T = any>(config?: AjaxCallbackConfig<T>): Request<void>
-    <T = any>(url?: string, data?: Data, config?: AjaxRequestConfig): Request<T>
+    <T = any>(config?: AjaxRequestConfig): Promise<T>
+    <T = any>(url?: string, data?: Data, config?: AjaxRequestConfig): Promise<T>
   }
 }
