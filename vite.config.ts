68210925d859946ab6005eb61b87eaffda7ac29f
wh <15996589@qq.com>
2023-04-18 11:47:44 +0800

    chore: dev不生成inspect

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,12 @@ import { buildTime } from './build/time'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
+  console.log('env', mode, env)
   const pUni = uni()
   const pImport = autoImport({ imports: ['vue', 'pinia'], dts: 'types/autoImport.d.ts' })
   const pUno = unocss()
   const pVisualizer = visualizer()
-  const pInspect = Inspect({ build: true, outputDir: 'dist/.vite-inspect' })
+  const pInspect = Inspect({ build: env.VITE_USER_NODE_ENV === 'production', outputDir: 'dist/.vite-inspect' })
   return {
     plugins: [pUni, pImport, pUno, buildTime, mpHooks, pVisualizer, pInspect],
     resolve: {
