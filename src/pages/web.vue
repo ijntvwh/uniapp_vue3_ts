3905e478205e810480189dc9d56a4d969ccb7cf2
wh <15996589@qq.com>
2023-12-07 14:46:11 +0800

    feat: web

---
+++ src/pages/web.vue
@@ -0,0 +1,21 @@
+<template>
+  <web-view :src="urlRef" class="screen" @error="onError" />
+</template>
+
+<script setup lang="ts">
+import { onLoad } from '@dcloudio/uni-app'
+const urlRef = ref()
+onLoad(options => {
+  urlRef.value = decodeURIComponent(options?.src)
+})
+function onError(e) {
+  console.log(e)
+}
+onMounted(() => {
+  if (!urlRef.value) {
+    console.log('url为空')
+  }
+})
+</script>
+
+<style scoped></style>
